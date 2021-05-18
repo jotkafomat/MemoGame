de6b5f36a758da70cbaff7d58683d631727ac4ba
jotkafomat <k-j@xl.wp.pl>
2021-05-18 18:07:53 +0100

    demo 1 & 2

--- MemoGame/ContentView.swift
+++ MemoGame/ContentView.swift
@@ -7,15 +7,101 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView {
+    
+    var emojis = ["🐸","🦆", "🐶","🦁", "🦒", "🦊", "🐼","🐎","🧸","🐽","🦖","🐢","🐓","🐳","🦀","🦑","🦐","🐷"]
+    @State var emojiCount = 18
+    
+}
+
+extension ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            ScrollView(showsIndicators: false) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                    }
+                }
+                .foregroundColor(.red)
+            }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+    }
+}
+
+extension ContentView {
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct CardView {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    let shape = RoundedRectangle(cornerRadius: 20)
+    
+}
+
+extension CardView: View {
+    
+    var body: some View {
+        ZStack {
+            
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+        .aspectRatio(2/3, contentMode: .fit)
+
     }
 }
