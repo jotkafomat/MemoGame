d317aba4520cb90a703eef04757aabdc860fc439
jotkafomat <k-j@xl.wp.pl>
2021-05-18 15:40:37 +0100

    Initial Commit

---
+++ MemoGame/MemoGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoGameApp.swift
+//  MemoGame
+//
+//  Created by Krzysztof Jankowski on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
